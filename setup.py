a6a7ea3d43dfc49f06442e4ed35be92047616c11
Teddy Morduhovich <34162644+swizex@users.noreply.github.com>
2018-08-10 16:18:02 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyTAPI",
-    version="0.2.3",
+    version="0.2.7",
     author="Teddy Morduhovich",
     author_email="swizex@gmail.com",
     description="a general purpose/random stuff api i made for myself",
