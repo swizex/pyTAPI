f54e0fc28b68dcc7d6a1fc69dc99cf197aa276d2
Teddy Morduhovich <34162644+swizex@users.noreply.github.com>
2018-08-10 13:20:49 +0300

    Add files via upload

---
+++ pyTAPI/__init__.py
@@ -0,0 +1,18 @@
+__all__ = ['pyTAPI']
+name = "pyTAPI"
+
+from pyTAPI import AESEncryption
+from pyTAPI import encrypt_data
+from pyTAPI import decrypt_data
+from pyTAPI import MysqlConnector
+from pyTAPI import GoogleMail
+from pyTAPI import binary_to_string
+from pyTAPI import string_to_binary
+from pyTAPI import generate_uuid
+from pyTAPI import generate_string_uuid
+from pyTAPI import generate_highlight
+from pyTAPI import markdown_converter
+from pyTAPI import tor_request
+from pyTAPI import generate_key
+from pyTAPI import generate_hash
+from pyTAPI import Tostring
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyTAPI",
-    version="0.1.4",
+    version="0.2.3",
     author="Teddy Morduhovich",
     author_email="swizex@gmail.com",
     description="a general purpose/random stuff api i made for myself",
